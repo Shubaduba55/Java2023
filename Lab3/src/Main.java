1fb9f9fa656da8004425190d290c36f11a293c6f
Bovvan <shamkov2323@gmail.com>
2023-11-04 13:10:45 +0200

    Lab 3 started
    Working in Lab3 branch

---
+++ Lab3/src/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Classname - Main
+ *
+ * Version info...
+ *
+ * Programmer(s): Ivan Shamkov (KPI, FICT, IP-11)
+ *
+ * Date: 04.11.2023
+ *
+ * Copyright notice...
+ *
+ * Description:
+ * Main class used for implementation of Lab 3.
+ * C3 = 34 % 3 = 1 (StringBuffer)
+ * C17 = 34 % 17 = 0 (Знайти найбільшу кількість речень заданого тексту, в яких є однакові слова.)
+ * */
+
+import java.util.Scanner;
+
+public class Main {
+    static public void main(String[] args){
+        
+    }
+}
